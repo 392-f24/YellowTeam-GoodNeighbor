1516fa92b3ecd756046a60a43a503fbf1bb7175c
Linh Ly <linhly2025@u.northwestern.edu>
2024-10-01 01:04:12 -0500

    Setup Firebase for Auth and DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
 import { useState } from 'react';
-import logo from './logo.svg';
+import { HashRouter } from 'react-router-dom';
+import Navigation from './components/Navigation.jsx';
 import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
+    <HashRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Navigation/>
       </header>
     </div>
+    </HashRouter>
   );
 };
 
---
+++ src/utilities/firebase.js
@@ -0,0 +1,83 @@
+import { getDatabase, onValue, ref, update, get } from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut as firebaseSignOut } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPJhTswV7OHI1weBX4_0dMqRSauCwUDJs",
+    authDomain: "yellowteam-goodneigh.firebaseapp.com",
+    databaseURL: "https://yellowteam-goodneigh-default-rtdb.firebaseio.com",
+    projectId: "yellowteam-goodneigh",
+    storageBucket: "yellowteam-goodneigh.appspot.com",
+    messagingSenderId: "157525304360",
+    appId: "1:157525304360:web:5e9cc1389851deb0d3011d",
+    measurementId: "G-VP1GR150E7"
+  };
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+const auth = getAuth(firebase);
+
+export const useDbData = (path) => {
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+
+    useEffect(() => (
+        onValue(ref(database, path), (snapshot) => {
+            setData(snapshot.val());
+        }, (error) => {
+            setError(error);
+        })
+    ), [path]);
+
+    return [data, error];
+};
+
+const makeResult = (error) => {
+    const timestamp = Date.now();
+    const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+    return { timestamp, error, message };
+};
+
+export const useDbUpdate = (path) => {
+    const [result, setResult] = useState();
+    const updateData = useCallback((value) => {
+        console.log('Updating path:', path);
+        console.log('Value before update:', value);
+
+        if (!value || typeof value !== 'object') {
+            console.error("Invalid value passed to updateData:", value);
+            return;
+        }
+
+        const dbRef = ref(database, path);
+        update(dbRef, value)
+            .then(() => setResult(makeResult()))
+            .catch((error) => {
+                console.error("Error during Firebase update:", error);
+                setResult(makeResult(error));
+            });
+    }, [path]);
+
+    return [updateData, result];
+};
+
+export { firebase, database, auth };
+
+export const signInWithGoogle = () => {
+    signInWithPopup(auth, new GoogleAuthProvider());
+};
+
+export const signOut = () => firebaseSignOut(auth);
+
+export const useAuthState = () => {
+    const [user, setUser] = useState();
+
+    useEffect(() => (
+        onAuthStateChanged(auth, setUser)
+    ), []);
+
+    return [user];
+};
+
