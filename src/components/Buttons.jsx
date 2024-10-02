4f8421697e69cadbaa8ac82373d87896eba37eca
Diana Whealan <dianawhealan@Dianas-MacBook-Air.local>
2024-10-02 14:07:10 -0500

    create a buttons file

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,17 @@
+import { signInWithGoogle, signOut, useAuthState } from '../utilities/firebase';
+
+const SignInButton = () => (
+    // <button className="button" onClick={signInWithGoogle}>Sign in</button>
+    <button type="button" class="btn btn-light" onClick={signInWithGoogle}>Sign in</button>
+  );
+  
+  const SignOutButton = () => (
+    <button className="button" onClick={signOut}>Sign out</button>
+  );
+  
+  const AuthButton = () => {
+    const [user] = useAuthState();
+    return user ? <SignOutButton /> : <SignInButton />;
+  };
+
+  export default AuthButton;
