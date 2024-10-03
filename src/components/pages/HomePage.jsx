aaffc07616c70737d27dec4ee7a88acf0d649420
Linh Ly <linhly2025@u.northwestern.edu>
2024-10-02 23:24:44 -0500

    Navigation Bar

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./HomePage.css"
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+       
+      <h1>HOMEPAGE</h1>
+    
+  </div>
+  )
+}
+
+export default HomePage;
---
+++ src/components/pages/RequestsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './RequestsPage.css'
+
+const RequestsPage = () => {
+  return (
+    <div className="requests-page">
+      <h1>Requests Page</h1>
+      <p>This is the Requests Page.</p>
+    </div>
+  );
+};
+
+export default RequestsPage;
