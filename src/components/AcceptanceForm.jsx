474934194cc4c3f4fafbba5c6c467c879984ad82
Diana Whealan <dianawhealan@Dianas-MacBook-Air.local>
2024-10-05 15:08:36 -0500

    modal with correct card information

--- src/components/AcceptanceForm.jsx
+++ src/components/AcceptanceForm.jsx
@@ -1,24 +1,43 @@
-import Form from 'react-bootstrap/Form';
+import {Form, Container, Button} from 'react-bootstrap';
 
 //default form 
-const AcceptanceForm = (request, user) => (
-    <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>{user.username}</Form.Label>
-        <Form.Control
-            type="email"
-            placeholder="name@example.com"
-            autoFocus
-        />
-        </Form.Group>
-        <Form.Group
-            className="mb-3"
-            controlId="exampleForm.ControlTextarea1"
-        >
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-    </Form>
-);
+const AcceptanceForm = () => {
+    return (
+        <Container className="mt-5">
+            <Form>
+                {/* Phone Number Input */}
+                <Form.Group className="mb-3" controlId="formPhoneNumber">
+                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Control
+                        className="w-100"
+                        type="phone number"
+                        placeholder="Phone number"
+                        autoFocus
+                    />
+                </Form.Group>
+                {/* Message Input */}
+                <Form.Group
+                    className="mb-3"
+                    controlId="formMessage"
+                >
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control 
+                        className="w-100"
+                        as="textarea" 
+                        rows={5} 
+                        style={{ minHeight: '100px' }}
+                        placeholder="Write your message"
+                    />
+                </Form.Group>
 
+                {/* Accept Button */}
+                <div className="d-flex justify-content-center">
+                    <Button variant="primary" type="submit">
+                        Accept Request
+                    </Button>
+                </div>
+            </Form>
+        </Container>
+    );
+}
 export default AcceptanceForm
