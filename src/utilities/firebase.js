2df7c1ccb6f812555b161e3d70289b685170ca9d
xuh5 <84805264+xuh5@users.noreply.github.com>
2024-10-06 17:43:41 -0500

    resolve merge conflict_2

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,4 +1,4 @@
-import { getDatabase, onValue, ref, update, get, push, set,remove } from 'firebase/database';
+import { getDatabase, onValue, ref, update, get, push, set ,remove} from 'firebase/database';
 import { useCallback, useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut as firebaseSignOut } from 'firebase/auth';
@@ -98,6 +98,11 @@ export const useDbAdd = (path) => {
     return [add, result];
   };
 
+  export const getRef = (path) => {
+
+
+
+  };
   export const useDbRemove = () => {
     const [result, setResult] = useState(null);
 
