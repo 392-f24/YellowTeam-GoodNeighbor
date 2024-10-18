6f7c110a8aab71136a6a75cccfb87d56d1a3e024
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2024-10-18 12:52:03 -0500

    revert button request change

--- src/components/buttons_request.jsx
+++ src/components/buttons_request.jsx
@@ -33,14 +33,11 @@ export const buttonCreate = (status, requestId, delivery_pref, withdrawHook, sta
     case 'Accepted':
       return (
         <>
-           <Button variant="success" size="sm" className="me-2" onClick={() => handleAcceptHelp(requestId,statusHook)}>
-            Accept Help
+          <Button variant="success" size="sm" className="me-2" onClick={() => modalhook(requestId)}>
+            Close Request
           </Button>
-          <Button variant="danger" size="sm" className="me-2" onClick={() => handleWithdrawHelp(requestId,statusHook,delivery_pref)}>
-            Reject Help
-          </Button>
-          <Button variant="info" size="sm" onClick={() => profileModalHook(requestId)}>
-            View Profile
+          <Button variant="danger" size="sm" onClick={() => handleWithdrawRequest(requestId,withdrawHook)}>
+            Withdraw Request
           </Button>
         </>
       );
