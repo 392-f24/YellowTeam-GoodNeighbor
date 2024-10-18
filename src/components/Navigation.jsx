8ee78bac625e6b8ead2b244b367e92392f2fbb5f
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2024-10-18 10:03:20 -0500

    added active state to icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,17 +9,17 @@ const Navigationbar = () => {
     <Navbar fixed="bottom" className="bg-light border-top">
       <Container className="d-flex justify-content-around">
         <Nav className="w-100 d-flex justify-content-between">
-          <Nav.Link as={NavLink} to="/" className="nav-icon">
+          <Nav.Link as={NavLink} to="/" className="nav-icon" activeClassName="active">
             <i className="bi bi-house-door-fill"></i>
             <p>Home</p>
           </Nav.Link>
 
-          <Nav.Link as={NavLink} to="/profile" className="nav-icon">
+          <Nav.Link as={NavLink} to="/profile" className="nav-icon" activeClassName="active">
             <i className="bi bi-person-fill"></i>
             <p>Profile</p>
           </Nav.Link>
 
-          <Nav.Link as={NavLink} to="/requests" className="nav-icon">
+          <Nav.Link as={NavLink} to="/requests" className="nav-icon" activeClassName="active">
             <i className="bi bi-list-task"></i>
             <p>Requests</p>
           </Nav.Link>
