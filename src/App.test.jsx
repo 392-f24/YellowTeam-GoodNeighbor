10a6051027ebab2a46981172dcb325f363c4b9ff
Diana Whealan <dianawhealan@Dianas-MacBook-Air.local>
2024-10-28 21:42:13 -0500

    second test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,12 @@
 import { describe, it, expect, vi,  test} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import '@testing-library/jest-dom';  // Import jest-dom matchers
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import RequestFormPage from './components/pages/RequestForm';
+import HomePage from './components/pages/HomePage';
 
 describe('RequestFormPage', () => {
   test('displays Meet Up textbox when Meet up option is selected', () => {
@@ -22,6 +24,27 @@ describe('RequestFormPage', () => {
     expect(input).toBeInTheDocument();
 
   })
-  
+
+  describe('send text from home page to request form', () => {
+    it('should send the text in the textbox to the textbox on the request form', () => {
+      render(
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/requestform" element={<RequestFormPage />} />
+          </Routes>
+        </MemoryRouter>
+      );
+      const input = screen.getByPlaceholderText(/How can your neighbors help?/i);
+      fireEvent.change(input, { target: { value: 'I need a hammer!' } });
+
+      const submitButton = screen.getByText(/Submit/i);
+      fireEvent.click(submitButton);
+
+      expect(screen.getByText(/New Request/i)).toBeInTheDocument();
+      expect(screen.getByLabelText(/Description/i)).toHaveValue('I need a hammer!');
+    });
+    
+  });
   
 })
